a022a7d85f2b25d2e8c1845dd61c09009c448c94
Daniel Horowitz <daniel.horowitz@openx.com>
2016-04-07 11:29:12 -0700

    Start migration to setuptools
    Move scripts to module
    Modify scripts to use main functions

---
+++ poewatcher/csv_sender.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+from poewatcher import config as c
+from poewatcher import utils
+
+
+def main():
+    """Use only once, to send a whole CSV file.
+
+    Ignores first line (assumed to be headers)
+    """
+    with open(c.get("map_recorder", "output_path"), "r", encoding='utf-8') as file:
+        file.readline()  # Go past first line[headers]
+        for line in file:
+            k = utils.contact_server(utils.dict_from_csv(line))
+            print("Received : {0}".format(k))
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
-from distutils.core import setup
-import os
+from setuptools import setup
 
-if os.name == "nt":
-    import py2exe
-
-DATA = [("", ["poewatcher/config.ini", "LICENSE", "README.md"]),
-        ("extras/", ["extras/error.wav", "extras/warning.wav"])]
 setup(
-    name='poewatcher',
-    packages=['poewatcher'],
-    scripts=['main.py', 'csv_sender.py'],
-    version='0.1b',
-    description='PoE log watcher',
-    author='Guillaume Dupuy',
-    author_email='glorf@glorf.fr',
-    url='https://github.com/Gloorf/watch_poe',
-    license='AGPL v3+',
-    keywords=['poe'],
+    name="poewatcher",
+    packages=["poewatcher"],
+    version="0.1b",
+    description="PoE log watcher",
+    author="Guillaume Dupuy",
+    author_email="glorf@glorf.fr",
+    url="https://github.com/Gloorf/poewatcher",
+    license="AGPL v3+",
+    keywords=["poe"],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
@@ -24,14 +17,14 @@ setup(
         "Operating System :: OS Independent",
         "Development Status :: 2 - Pre-Alpha",
     ],
-    # py2exe stuff
-    console=["main.py"],
-    data_files=DATA,
-    options={
-        "py2exe": {
-            "packages": ["poewatcher", "pyglet"],
-            "bundle_files": 2,
-            "skip_archive": True
-        }
+    install_requires=["click", "configparser", "pyglet", "pyperclip", "requests"],
+    entry_points={
+        "console_scripts": [
+            "poewatcher = poewatcher.main:main",
+            "poewatcher-sendcsv = poewatcher.send_csv:main"
+        ]
+    },
+    package_data={
+        "poewatcher": ["config.ini", "data/*"]
     }
 )
